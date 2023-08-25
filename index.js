e6befbf19cc20201686fabb881c46b41ee4e5d70
tjtjaiswal <143197150+tjtjaiswal@users.noreply.github.com>
2023-08-25 20:49:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// const btn = document.querySelector("button");
+
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+
+// function bgChange(e) {
+//   const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+//   e.target.style.backgroundColor = rndCol;
+//   console.log(e);
+// }
+
+// btn.addEventListener("click", bgChange);
+
+
+
+
+
+
+
+
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function changeBackground() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.addEventListener("click", changeBackground);
